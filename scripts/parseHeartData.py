fbbef5901421e6dbc0025fe8b0516491d7d5374e
Luke Fraser <lukefrasera@gmail.com>
2014-11-30 18:31:03 -0800

    adding support for packing

--- scripts/parseHeartData.py
+++ scripts/parseHeartData.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import re, sys
+import re, sys, struct
 
 def main():
   heart_file = open(sys.argv[1], 'r')
@@ -17,6 +17,13 @@ def main():
   for i in xrange(len(heart_val)):
     heart_val[i] = [timestamp[0] + delta*i, heart_val[i]]
 
-  
+  heart_file.close()
+
+  output = open(sys.argv[2], 'wb')
+  fmt = 'di'
+  output.write(fmt.ljust(25,' '))
+  for val in heart_val:
+    output.write(struct.pack(fmt, *val))
+  output.close()
 if __name__ == '__main__':
   main()
