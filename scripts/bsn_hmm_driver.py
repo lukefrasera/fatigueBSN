b6d0c6d4b574ea0b07561673cb0264da3791b2c8
Terence Henriod <thenriod@gmail.com>
2014-12-14 00:38:04 -0800

    worked on BSNDataPoint

--- scripts/bsn_hmm_driver.py
+++ scripts/bsn_hmm_driver.py
@@ -4,8 +4,8 @@ __author__ = 't'
 
 import collections
 import struct
+from bsn_data_point import BSN_EMISSION_ALPHABET, BSNDataPoint
 from ghmm import *
-from fatigueBSN_model.BSNDataPoint import *
 
 DATA_FORMAT_STRING = 'ddddddddd'
 
@@ -26,7 +26,7 @@ def main():
     print training_labels
 
     # aka sigma
-    emission_aplphabet = EMISSION_ALPHABET
+    emission_aplphabet = BSN_EMISSION_ALPHABET
 
     # aka pi
     initial_state_model = [0.9,  # non-fatigue
@@ -35,27 +35,50 @@ def main():
     state_transition_matrix = [[0.7, 0.3],    # non-fatigue
                                [0.1, 0.9]]    # fatigue
 
-    non_fatigue_emission_probabilities = [
-        0.3,   # 'aaa'
-        0.15,  # 'aab'
-        0.1,   # 'aba'
-        0.1,   # 'abb'
-        0.1,   # 'baa'
-        0.1,   # 'bab'
-        0.1,   # 'bba'
-        0.05   # 'bbb'
-    ]
+    non_fatigue_emission_probabilities = \
+        [
+            0.01,  # aaaa
+            0.01,  # aaab
+            0.01,  # aaba
+            0.01,  # aabb
+            0.01,  # abaa
+            0.01,  # abab
+            0.01,  # abba
+            0.01,  # abbb
+            0.01,  # baaa
+            0.01,  # baab
+            0.01,  # baba
+            0.01,  # babb
+            0.01,  # bbaa
+            0.01,  # bbab
+            0.01,  # bbba
+            0.01   # bbbb
+        ]
 
-    fatigue_emission_probabilities = [
-        0.1,  # 'aaa'
-        0.1,  # 'aab'
-        0.1,  # 'aba'
-        0.1,  # 'abb'
-        0.1,  # 'baa'
-        0.1,  # 'bab'
-        0.1,  # 'bba'
-        0.3   # 'bbb'
-    ]
+    fatigue_emission_probabilities = \
+        [
+            0.01,  # aaaa
+            0.01,  # aaab
+            0.01,  # aaba
+            0.01,  # aabb
+            0.01,  # abaa
+            0.01,  # abab
+            0.01,  # abba
+            0.01,  # abbb
+            0.01,  # baaa
+            0.01,  # baab
+            0.01,  # baba
+            0.01,  # babb
+            0.01,  # bbaa
+            0.01,  # bbab
+            0.01,  # bbba
+            0.01   # bbbb
+        ]
+
+    if not probabilities_sum_to_one(non_fatigue_emission_probabilities):
+        print 'FIX THE NON-FATIGUE PROBS!!!!'
+    if not probabilities_sum_to_one(fatigue_emission_probabilities):
+        print 'FIX THE FATIGUE PROBS!!!!'
 
     # aka B
     emission_likelihood_matrix = [non_fatigue_emission_probabilities,
@@ -73,14 +96,14 @@ def main():
     )
 
     test_list = ['aaa', 'baa', 'bba', 'bbb', 'bbb', 'bbb', 'bbb', 'bbb', 'bbb']
-    test_emission = EmissionSequence(EMISSION_ALPHABET, test_list)
+    test_emission = EmissionSequence(BSN_EMISSION_ALPHABET, test_list)
     print "State estimation on test_list before training:"
     print bsn_hmm_model.viterbi(test_emission)
 
     # retrain the probablilties of the model
     print "Re-training model..."
     bsn_hmm_model.baumWelch(
-        EmissionSequence(EMISSION_ALPHABET, training_emissions)
+        EmissionSequence(BSN_EMISSION_ALPHABET, training_emissions)
     )
     # print bsn_hmm_model
 
@@ -90,19 +113,7 @@ def main():
     # test_labels = bsn_hmm_model.viterbi(test_emission)
     # print test_labels
     # print determine_most_likely_state(test_labels[0])
-
-
-def determine_most_likely_state(state_labels):
-    label_counts = collections.defaultdict(lambda: 0)
-
-    most_frequent_label = label_counts[0]
-    high_frequency = 0
-    for label in state_labels:
-        if label_counts[label] > high_frequency:
-            most_frequent_label = label
-            high_frequency = label_counts[label]
-
-    return most_frequent_label
+    # print determine_percent_correct( ... )
 
 
 def unpack_binary_data_into_list(file_name):
@@ -115,6 +126,7 @@ def unpack_binary_data_into_list(file_name):
             print "data: {}".format(data_tuple)
             data_points.append(BSNDataPoint(
                 data_tuple[0],  # timestamp
+                data_tuple[1],
                 data_tuple[4],  # alpha frequency
                 data_tuple[1],  # attention level
                 data_tuple[3]   # torso position TODO: testing using arbitrary
@@ -132,6 +144,35 @@ def packed_structs_from_file(filename, struct_size):
             else:
                 break
     yield None
+
+
+def probabilities_sum_to_one(probability_vector):
+    sum = 0.0
+    for probability in probability_vector:
+        sum += probability
+
+    return sum == 1.0
+
+def determine_percent_correct(input_list, output_list):
+    num_correct = 0
+    for i in range(0, len(input_list)):
+        if input_list[i] == output_list[i]:
+            num_correct += 1
+
+    return float(num_correct) / float(len(input_list))
+
+
+def determine_most_likely_state(state_labels):
+    label_counts = collections.defaultdict(lambda: 0)
+
+    most_frequent_label = label_counts[0]
+    high_frequency = 0
+    for label in state_labels:
+        if label_counts[label] > high_frequency:
+            most_frequent_label = label
+            high_frequency = label_counts[label]
+
+    return most_frequent_label
 
 if __name__ == "__main__":
     main()
